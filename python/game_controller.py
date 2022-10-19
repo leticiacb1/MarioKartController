e43a02692e6066342fba03f10c8f8a2dad9eeea9
leticiacb1 <leticiacb1@al.insper.edu.br>
2022-10-19 17:30:44 -0300

    Subindo python imu funcionando. Co-authored-by: lorrancmlopes <lorrancml@al.insper.edu.br>

--- python/game_controller.py
+++ python/game_controller.py
@@ -12,7 +12,7 @@ class SerialControllerInterface:
 
     # //                PROTOCOLO
 	# // ----------------------------------------------
-	# // tipo de dado -> A (analogico) D (Digital)
+	# // tipo de dado -> A (analogico) D (Digital) I (IMU)
 	# // id_botão     -> botão azul 'A'   botão verde 'B'
 	# // status botão -> '1' pressionado  '0' soltou
 	
@@ -95,6 +95,22 @@ class SerialControllerInterface:
         if y== b'b':
             self.j.set_button(self.mapping.button['DOWN'], 1) 
 
+    def input_imu_action(self,speed_state):
+        print("------\n")
+        print('\n >> IMU\n')
+        print(speed_state);
+        print("------\n")
+        if(speed_state == 'r'):
+            logging.info("\nRight\n")
+            self.j.set_button(self.mapping.button['RIGHT'], 1)  
+        elif(speed_state == 'l'):
+            logging.info("\nLeft\n")
+            self.j.set_button(self.mapping.button['LEFT'], 1)
+        else:
+            logging.info("Para")
+            self.j.set_button(self.mapping.button['RIGHT'], 0)
+            self.j.set_button(self.mapping.button['LEFT'], 0)
+
     def update(self):
 
         ## Sync protocol
@@ -132,6 +148,17 @@ class SerialControllerInterface:
         if data_type == b'J':
             #                       eixo_x    eixo_y
              self.input_vjoy_values(id_state, status)
+
+        # IMU value
+        if data_type == b'I':
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            print(id_state)
+            if(id_state == b'r'):
+                self.input_imu_action('r')
+            elif(id_state == b'l'):
+                self.input_imu_action('l')
+            elif(id_state == b's'):
+                self.input_imu_action('s')
                 
         self.incoming = self.ser.read()
 
